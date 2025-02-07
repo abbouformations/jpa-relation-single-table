8809483247b1e31bdce548301d55c4cbb84c7392
abbou <abbouformations@gmail.com>
2025-02-07 23:35:38 +0100

    FC

---
+++ src/main/java/ma/formations/jpa/service/model/Professeur.java
@@ -0,0 +1,20 @@
+package ma.formations.jpa.service.model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@DiscriminatorValue("PROF")
+public class Professeur extends Personne {
+	private String diplome;
+
+	public Professeur(String nom, String prenom, String diplome) {
+		super(nom, prenom);
+		this.diplome = diplome;
+	}
+}
